bfed3e1c5f3e388d4dce5c33c89e83b12f61678b
Mateo Astudillo <ast.mateo@gmail.com>
2024-09-27 18:41:41 -0300

    setup

---
+++ setup/insert-candidate.sql
@@ -0,0 +1,9 @@
+SET NAMES utf8mb4;
+INSERT INTO `Candidate`
+    (`president_first_name`, `president_last_name`, `vicepresident_first_name`, `vicepresident_last_name`,
+    `political_party`, `list`) VALUES
+    ('Javier', 'Milei', 'Victoria', 'Villarruel', 'Libertad Avanza', '135A'),
+    ('Sergio', 'Massa', '', '', 'Unión por la Patria', '134A'),
+    ('Patricia', 'Bullrich', '', '', 'Juntos por el Cambio', '132B'),
+    ('Juan', 'Schiaretti', '', '', 'Hacemos por Nuestro País', '133A'),
+    ('Myriam', 'Bregman', '', '', 'Frente de Izquierda', '136A');
